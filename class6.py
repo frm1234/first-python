ec34c39c9ea8295162ea168858e6b644df56a0c5
frm1234 <136208541+frm1234@users.noreply.github.com>
2023-08-05 03:57:33 +0000

    modify

--- class6.py
+++ class6.py
@@ -1,14 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import streamlit as st
 
+st.slider('三角函數',min_value=0,max_value=10)#網頁建立調整拉條
 t = np.arange(0.,1.0,0.05)
-print(t)
 y1 = np.sin(2 * np.pi * t)
 y2 = np.cos(2 * np.pi * t)
-print(y1)
-print(y2)
 figure1 = plt.figure(figsize=(8,4))
 axes1 = figure1.add_subplot()
 axes1.plot(y1)
 axes1.plot(y2)
-plt.show()
+st.write(figure1)
